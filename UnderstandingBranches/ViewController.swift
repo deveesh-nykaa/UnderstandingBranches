e90df326985411073e67fc57f0833a293da86217
Deveesh Mehandiratta <deveesh.mehandiratta@nykaa.com>
2021-01-29 13:11:54 +0530

    Initial Commit

---
+++ UnderstandingBranches/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnderstandingBranches
+//
+//  Created by Deveesh Mehandiratta on 29/01/21.
+//  Copyright © 2021 Deveesh Mehandiratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
