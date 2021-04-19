853608f1d6d3849aba8f5ff542ca047fd5881cf7
Deveesh Mehandiratta <deveesh.mehandiratta@nykaa.com>
2021-04-19 12:26:52 +0530

    f2: 2

--- UnderstandingBranches/ViewController.swift
+++ UnderstandingBranches/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func f2() {
+        //1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
